25cb62c1cf742a094f2acfcc49253f8b3ad33925
Anteneh Bedilu <bediluanteneh@gmail.com>
2018-05-29 11:21:48 -0700

    WI_FI hotspot first commit

---
+++ start_wifi.bat
@@ -0,0 +1,7 @@
+echo Starting WI-FI hotspot 
+color 0a
+netsh wlan set hostednetwork mode=allow ssid="WI-FI hotspot" key="P@$$w0rd"
+netsh wlan start hostednetwork
+pause
+echo SSID or WI-FI name is WI-FI hotspot
+echo KEY or password for the WI-FI is P@$$w0rd
